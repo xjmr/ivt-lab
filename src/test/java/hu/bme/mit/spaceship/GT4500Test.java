d2d1912c7cf5ba1794f8ac0bea2e26159ba53975
xjmr <xjmr5cznlxls@opayq.com>
2018-05-11 03:34:54 -0700

    add tests for GT4500 using mockito

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -7,34 +7,48 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class GT4500Test {
-
   private GT4500 ship;
+  private TorpedoStore primaryTorpedoStore;
+  private TorpedoStore secondaryTorpedoStore;
 
   @Before
   public void init(){
-    this.ship = new GT4500();
+    primaryTorpedoStore = mock(TorpedoStore.class);
+    secondaryTorpedoStore = mock(TorpedoStore.class);
+    this.ship = new GT4500(primaryTorpedoStore, secondaryTorpedoStore);
+  }
+
+  private void setupTorpedoStoreMock(TorpedoStore ts) {
+    when(ts.isEmpty()).thenReturn(false);
+    when(ts.fire(1)).thenReturn(true);
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
+    setupTorpedoStoreMock(primaryTorpedoStore);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
 
     // Assert
     assertEquals(true, result);
+    verify(primaryTorpedoStore, times(1)).fire(1);
   }
 
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
+    setupTorpedoStoreMock(primaryTorpedoStore);
+    setupTorpedoStoreMock(secondaryTorpedoStore);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.ALL);
 
     // Assert
     assertEquals(true, result);
+    verify(primaryTorpedoStore, times(1)).fire(1);
+    verify(secondaryTorpedoStore, times(1)).fire(1);
   }
 
 }
